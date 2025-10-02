aa7f07cc374f4eb1a7fb54c77dd326cd0518f2f8
Olimjon Murtazoyev <murtazoyevolimjon03gmail.com>
2025-10-02 13:39:23 +0500

    done

--- db.sql
+++ db.sql
@@ -56,3 +56,39 @@ create table match_fixtures (
     foreign key (away_team_id) references teams(team_id),
     foreign key (tournament_id) references tournaments(tournament_id)
 );
+
+
+
+
+
+
+
+
+
+create table type_practice (
+    id serial primary key,
+    full_name varchar(100) not null,
+    age int check (age > 0),
+    salary numeric(10,2),
+    is_active boolean default true,
+    birth_date date,
+    created_at timestamp default now(),
+    login_time time,
+    ip_address inet,
+    mac_address macaddr,
+    doc bytea,
+    data_json json,
+    tags text[],
+    unique_code uuid default gen_random_uuid()
+);
+
+insert into type_practice (full_name, age, salary, is_active, birth_date, login_time, ip_address, mac_address, data_json, tags)
+values
+('ali valiyev', 25, 1200.50, true, '2000-05-10', '08:30:00', '192.168.1.10', '08:00:27:12:34:56', '{"lang":"uz","level":"beginner"}', array['postgres','sql','practice']),
+('dilnoza karimova', 30, 2500.00, false, '1995-03-22', '09:45:00', '10.0.0.1', 'aa:bb:cc:dd:ee:ff', '{"lang":"en","level":"intermediate"}', array['coding','database']);
+
+
+
+
+
+
